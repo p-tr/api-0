d41c428bd006b08893e54eef51fa51f9be9842ff
Pierre Tellier-Robert <hi@p-tr.space>
2022-02-01 13:04:16 +0100

    Added JWT support

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const app = express();
 const { connect, model, Schema } = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
+const jwt = require('jsonwebtoken');
+
+const cookieParser = require('cookie-parser');
+
+const secret = 'ChangeMe!';
+
 // Middlewares
 
 // 406 & 415 status code middleware
@@ -23,7 +29,48 @@ app.use((req, res, next) => {
   next();
 });
 
+// Parse HTTP cookies
+app.use(cookieParser());
+
+// Parse HTTP request body
 app.use(express.json());
+
+// Middleware d'authentification
+app.use((req, res, next) => {
+  // les cookies sont dans req.cookies
+  // l'en tête d'autorisation est dans req.get('authorization')
+
+  res.locals.user = null;
+
+  const data = [
+    req.cookies.authorization,
+    req.get('authorization')
+  ].map((item) => {
+    if(item) {
+      const splitted = item.trim().split(' ');
+      const token = splitted.length > 1
+        ? splitted[splitted.length - 1]
+        : splitted[0];
+
+      try {
+        const decoded = jwt.verify(token, secret);
+        return decoded;
+      } catch(err) {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  });
+
+  const session = data.find((element) => {
+    return (typeof element === 'object');
+  })
+
+  res.locals.user = session ? { _id: session.sub } : null;
+
+  next();
+});
 
 /*
   Il s'agit de mettre en oeuvre une API ultra-basique avec
@@ -59,6 +106,10 @@ app.use(express.json());
     GET /movies/:id : information d'un film par ID
     PUT /movies/:id : mettre à jour un film (par ID)
     DELETE /movies/:id : suppression d'un film (par ID)
+
+    POST /auth/token : authentification
+    DELETE /auth/token : désauthentification
+    GET /auth/token : décodage du token d'authentification
 
 */
 
@@ -215,10 +266,41 @@ connect('mongodb://localhost:27017/api').then(() => {
     }
   });
 
+  app.post('/auth/token', (req, res) => {
+    const { username = "", password = "" } = req.body;
+    const expiresIn = 3600;
+
+    // En temps normal, le processus d'authentification est implémenté ici.
+
+    const token = jwt.sign(
+      { sub: username },
+      secret,
+      // HS256 = HMAC SHA 256 => symétrique
+      // RS512 = RSA SHA 512 => asymétrique
+      { algorithm: 'HS256', expiresIn }
+    );
+
+    res.cookie('authorization', token, { httpOnly: true });
+    res.set('Authorization', token);
+
+    res.status(201).json({
+      token,
+      expiresIn
+    });
+  });
+
+  app.get('/user', (req, res) => {
+    res.json(res.locals.user);
+  });
+
+  app.delete('/auth/token', (req, res) => {
+    res.clearCookie('authorization');
+  });
+
   app.listen(3000, () => {
     console.log('Listening on :3000...');
   });
 
 }).catch((err) => {
-  console.log('Mongoose connection error');
+  console.log(err);
 })
